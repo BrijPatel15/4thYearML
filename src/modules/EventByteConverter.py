6c04b6f6178580268de0024ef3b3b5ce453d79da
BrijPatel15 <brij.15@live.ca>
2019-07-07 15:24:42 -0400

    Changed layout of file

--- src/modules/EventByteConverter.py
+++ src/modules/EventByteConverter.py
@@ -1,29 +1,25 @@
-import music21
-import MidiParser 
+import MidiParser
 
-def dataFrameToByteConverter(events):
-    totalRows = len(events.index)
-    finalByteArray = bytearray()
+def dataFrameToByteConverter(event):
     acceptedValues = ["event", "name"]
     for val in acceptedValues:
-        assert(val in str(events.columns.values))
-
-    for x in range(1, totalRows):
-        for value in acceptedValues:
-            finalByte = b'\xFF'
-            rowVal = events.ix[x, value]
-            if rowVal is 'Note':
-                finalByte = finalByte[0] & b'\x4F'[0]
-            if rowVal is 'Chord':
-                finalByte = finalByte[0] & b'\x2F'[0]
-            else:
-                finalByte = finalByte[0] & getByteFromNote(rowVal)
-            finalByteArray.append(finalByte)
-    return finalByteArray
+        assert(val in str(event.columns.values))
+    finalByte = b'\xFF'
+    for value in acceptedValues:
+        rowVal = event.at[0,value]
+        if rowVal is 'Note':
+            finalByte = finalByte[0] & b'\x4F'[0]
+        if rowVal is 'Chord':
+            finalByte = finalByte[0] & b'\x2F'[0]
+        else:
+            finalByte = finalByte[0] & getByteFromNote(rowVal)
+    return finalByte
         
 def getByteFromNote(note):
     noteVals = {
     }
     return noteVals.get(note)
 
-dataFrameToByteConverter(MidiParser.parse_notes('../music/John_Denver_-_Take_Me_Home_Country_Roads.mid'))
+events = MidiParser.parse_notes('/music/John_Denver_-_Take_Me_Home_Country_Roads.mid')
+for event in events:
+    dataFrameToByteConverter(event)
