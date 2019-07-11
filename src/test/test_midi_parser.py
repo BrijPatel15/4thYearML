850ac4a9116e09876a140194839a15b72768b17e
BrijPatel15 <brij.15@live.ca>
2019-07-11 19:21:53 -0400

    Added test cases for byte conversion

--- src/test/test_midi_parser.py
+++ src/test/test_midi_parser.py
@@ -1,6 +1,9 @@
 import unittest
 import sys
+import pandas as pd
 import MidiParser as midi
+import EventByteConverter as eventConverter
+from ByteConstTable import *
 
 class MidiParserTestCase(unittest.TestCase):
 
@@ -32,6 +35,43 @@ class MidiParserTestCase(unittest.TestCase):
         expected = False
         actual = midi.validate_file(BAD_MIDI_PATH_2)
         print(BAD_MIDI_PATH_2)
+        print("Expected: "+ str(expected) + " ==> Actual: "+str(actual))
+        self.assertEqual(expected, actual)
+
+    def test_EventConverter(self):
+        goodEvent = pd.DataFrame(dict(event="Note", name="C1"))
+        badEvent = pd.DataFrame(dict(event="Note", name="fail"))
+        goodChord = pd.DataFrame(dict(event="Chord", name=["C1", "B1"]))
+        badChord = pd.DataFrame(dict(event="Chord", name=[]))
+        badOverAll = pd.DataFrame(dict(event="fail" name="fail"))
+
+        expected = True
+        actual = validateValues(goodEvent.loc["event"], goodEvent.loc["name"])
+        res = eventConverter.dataFrameToByteConverter(goodEvent)
+        print("Expected: "+ str(expected) + " ==> Actual: "+str(actual))
+        self.assertEqual(expected, actual)
+        self.assertEqual(res[0], getByteFromNote("C1")[0])
+
+        expected = True
+        actual = validateValues(goodChord.loc["event"], goodChord.loc["name"])
+        res = eventConverter.dataFrameToByteConverter(goodChord)
+        print("Expected: "+ str(expected) + " ==> Actual: "+str(actual))
+        self.assertEqual(expected, actual)
+        self.assertEqual(res[0], getByteFromNote("C1")[0])
+        self.assertEqual(res[1], getByteFromNote("B1")[0])
+
+        expected = False
+        actual = validateValues(badEvent.loc["event"], badEvent.loc["name"])
+        print("Expected: "+ str(expected) + " ==> Actual: "+str(actual))
+        self.assertEqual(expected, actual)
+
+        expected = False
+        actual = validateValues(badChord.loc["event"], badChord.loc["name"])
+        print("Expected: "+ str(expected) + " ==> Actual: "+str(actual))
+        self.assertEqual(expected, actual)
+        
+        expected = False
+        actual = validateValues(badOverAll.loc["event"], badOverAll.loc["name"])
         print("Expected: "+ str(expected) + " ==> Actual: "+str(actual))
         self.assertEqual(expected, actual)
 
